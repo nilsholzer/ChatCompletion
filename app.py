e289f3736261538cc876032ba35e86a70ae97870
Nils Holzer <kroko.holzer@gmail.com>
2024-12-10 22:20:48 +0100

    Added comments

--- app.py
+++ app.py
@@ -3,21 +3,23 @@ from openai import OpenAI
 
 app = Flask(__name__)
 
-#OpenAI API Key
+#Gets the Open API key from the file API_KEY
 API_KEY = open("API_KEY", "r").read()
-
 
 client = OpenAI(api_key=API_KEY)
 
-
+#Renders the index.html file
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#Chat route that takes in a POST request with a JSON object containing the message
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
     data = request.get_json()
     message = data.get("message")
+
+    #Generates the response from the AI
     def generate():
         stream = client.chat.completions.create(
             model = "gpt-4o-mini",
@@ -28,6 +30,7 @@ def chat():
             stream=True
         )
 
+        #Yields every partial response from the AI until the final response is reached
         for chunk in stream:
             if chunk.choices[0].delta.content is not None:
                 yield(chunk.choices[0].delta.content)
